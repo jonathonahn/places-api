ed9c51d96b8ffe7acab7ff36056c86c3872556c5
jonathon <ahnjon77@gmail.com>
2021-10-05 14:40:09 -0700

    adds backend CRUD routes and controller methods

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,43 @@
 class PlacesController < ApplicationController
+  def index
+    places = Place.all 
+    render json: places 
+  end
+
+  def create
+    place = Place.new(
+      name: "#{params[:name]}",
+      address: "#{params[:address]}"
+    )
+    if place.save 
+      render json: place
+    else
+      render json: place.errors.full_messages
+    end
+  end
+
+  def show
+    place = Place.find(params[:id])
+    render json: place
+  end
+
+  def update
+    place = Place.find(params[:id])
+    place.name = params[:name] || place.name 
+    place.address = params[:address] || place.address
+    if place.save 
+      render json: place
+    else
+      render json: place.errors.full_messages
+    end
+  end
+
+  def destroy
+    place = Place.find(params[:id])
+    if place.destroy 
+      render json: {message: "place destroyed"}
+    else 
+      render json: place.errors.full_messages
+    end
+  end
 end
